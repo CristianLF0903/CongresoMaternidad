0f54ec58db155662ea7374809f3994af52f28bd4
Tianlon <clondonof@unal.edu.co>
2024-05-11 18:37:35 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -11,32 +11,3 @@ document
   .addEventListener("click", function () {
     document.getElementById("Sidebar").classList.toggle("translate-x-full");
   });
-
-// import Swiper bundle with all modules installed
-import Swiper from "swiper/bundle";
-
-// import styles bundle
-import "swiper/css/bundle";
-
-// init Swiper:
-const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "vertical",
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-});
